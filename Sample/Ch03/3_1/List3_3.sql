99802e88a291ad60df4651bbe4069b5b6e202f4f
kawashima-nagisa <nagisakawashima006@gmail.com>
2025-01-29 21:57:49 +0900

    NULL値を含むテーブルの作成とカウントクエリを追加

--- Sample/Ch03/3_1/List3_3.sql
+++ Sample/Ch03/3_1/List3_3.sql
@@ -1,2 +1,20 @@
 SELECT COUNT(*), COUNT(col_1)
   FROM NullTbl;
+
+CREATE TABLE NullTbl (
+    col_1 INTEGER
+);
+
+
+INSERT INTO NullTbl (col_1) VALUES 
+    (NULL),
+    (NULL),
+    (NULL);
+
+SELECT COUNT(*), COUNT(col_1) FROM NullTbl;
+
+
+/*
+COUNT(*) = 3 → すべての行をカウント
+COUNT(col_1) = 0 → NULL のデータはカウントしないため0になる
+*/
