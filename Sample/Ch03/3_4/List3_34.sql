a16f96383ce2a9e2496ad08d7e243dc1fe140eeb
kawashima-nagisa <nagisakawashima006@gmail.com>
2025-01-31 12:28:40 +0900

    SQLクエリの修正: 商品分類ごとのカウントと合計販売価格の取得、並び順の追加

--- Sample/Ch03/3_4/List3_34.sql
+++ Sample/Ch03/3_4/List3_34.sql
@@ -2,3 +2,9 @@ SELECT shohin_bunrui, COUNT(*)
   FROM Shohin
  GROUP BY shohin_bunrui
  ORDER BY COUNT(*);
+
+
+SELECT shohin_bunrui
+  FROM Shohin
+ GROUP BY shohin_bunrui
+ ORDER BY COUNT(*);
--- Sample/Ch03/3_4/List3_35.sql
+++ Sample/Ch03/3_4/List3_35.sql
@@ -1,9 +1,15 @@
--- —ñ–¼‚ÅŽw’è
+-- ï¿½ñ–¼‚ÅŽwï¿½ï¿½
 SELECT shohin_id, shohin_mei, hanbai_tanka, shiire_tanka
   FROM Shohin
  ORDER BY hanbai_tanka DESC, shohin_id;
 
--- —ñ”Ô†‚ÅŽw’è
+-- ï¿½ï¿½Ôï¿½ï¿½ÅŽwï¿½ï¿½
 SELECT shohin_id, shohin_mei, hanbai_tanka, shiire_tanka
   FROM Shohin
  ORDER BY 3 DESC, 1;
+
+
+ SELECT shohin_bunrui, SUM(hanbai_tanka) , SUM(shiire_tanka) 
+   FROM Shohin
+    GROUP BY shohin_bunrui
+  HAVING SUM(hanbai_tanka) >SUM(shiire_tanka)  *1.5;
