021a6986be39f4b22641562b6affe12ae0e0d2e7
kawashima-nagisa <nagisakawashima006@gmail.com>
2025-01-31 10:08:08 +0900

    AVG関数の使用に関するGROUP BYの必要性についてのコメントを追加

--- Sample/Ch03/3_3/List3_22.sql
+++ Sample/Ch03/3_3/List3_22.sql
@@ -1,3 +1,19 @@
 SELECT shohin_bunrui, AVG(hanbai_tanka)
   FROM Shohin
  GROUP BY shohin_bunrui;
+
+
+
+/*
+SQL では、集約関数 (AVG(), SUM(), COUNT() など) を使う場合、通常は GROUP BY が必要になります。
+GROUP BY shohin_bunrui によって、shohin_bunrui ごとにデータがグループ化 される。
+各グループに対して AVG(hanbai_tanka) が計算される。
+
+
+
+GROUP BY なしで AVG() を使う方法
+もし GROUP BY なしで AVG() を使いたいなら、すべての hanbai_tanka の平均だけを計算すればよい：
+
+
+SELECT AVG(hanbai_tanka) FROM Shohin;
+*/
