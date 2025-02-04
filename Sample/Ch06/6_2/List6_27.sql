dfaff7a45304dc88a2ffa84dc55cec7d28943e7c
kawashima-nagisa <nagisakawashima006@gmail.com>
2025-02-04 22:11:28 +0900

    SQLクエリの修正: ShohinテーブルのSELECT文に結果表示のコメントを追加

--- Sample/Ch06/6_2/List6_27.sql
+++ Sample/Ch06/6_2/List6_27.sql
@@ -1,3 +1,13 @@
 SELECT shohin_mei, hanbai_tanka
   FROM Shohin
  WHERE hanbai_tanka BETWEEN 100 AND 1000;
+
+
+ /*
+   shohin_mei  | hanbai_tanka 
+--------------+--------------
+ Tシャツ      |         1000
+ 穴あけパンチ |          500
+ フォーク     |          500
+ おろしがね   |          880
+ ボールペン   |          100
--- Sample/Ch06/6_2/List6_28.sql
+++ Sample/Ch06/6_2/List6_28.sql
@@ -2,3 +2,10 @@ SELECT shohin_mei, hanbai_tanka
   FROM Shohin
  WHERE hanbai_tanka > 100 
    AND hanbai_tanka < 1000;
+
+/*
+    shohin_mei  | hanbai_tanka 
+--------------+--------------
+ 穴あけパンチ |          500
+ フォーク     |          500
+ おろしがね   |          880
