38647e403ddd13cc6890927e1c8c6b52845df882
kawashima-nagisa <nagisakawashima006@gmail.com>
2025-02-04 10:59:27 +0900

    SQLクエリの修正: AvgTankaByBunruiビューの作成と平均販売単価の計算を追加

--- Sample/Ch05/5_3/List5_16_SQLServer_DB2_PostgreSQL_MySQL.sql
+++ Sample/Ch05/5_3/List5_16_SQLServer_DB2_PostgreSQL_MySQL.sql
@@ -11,3 +11,17 @@ SELECT shohin_bunrui, shohin_mei, hanbai_tanka
 相関サブクエリとは？	サブクエリの中で、外側のクエリの値（S1.shohin_bunrui）を参照する！
 なぜ相関サブクエリが必要？	分類ごとに異なる平均値を比較するため！
 通常のサブクエリとの違い	通常のサブクエリは1回だけ実行、相関サブクエリは各行ごとに実行！
+
+*/
+CREATE VIEW AvgTankaByBunrui AS
+select shohin_id, shohin_mei, shohin_bunrui, hanbai_tanka,
+(select AVG(hanbai_tanka)from Shohin S2 WHERE S1.shohin_bunrui = S2.shohin_bunrui GROUP BY shohin_bunrui) AS avg_hanbai_tanka
+FROM Shohin S1;
+
+
+ select * from AvgTankaByBunrui;
+
+
+ SELECT * 
+FROM AvgTankaByBunrui
+WHERE hanbai_tanka > avg_hanbai_tanka;
